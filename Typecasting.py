d23526ad62b0cdd959b950156ac5a48b0781a52a
Akshaj6 <akshaj.samaria@gmail.com>
2025-03-29 10:50:58 +0530

    5

---
+++ Typecasting.py
@@ -0,0 +1,14 @@
+name = "Riya"
+age = 173
+is_student = True
+weight = 43.6
+print("\n After Type Casting...")
+age = str(age)
+print(age)
+print("The data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("The data type of weight is", type(weight))
+is_student = str(is_student)
+print(is_student)
+print("The data type of is_student is", type(is_student))
