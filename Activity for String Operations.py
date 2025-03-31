7d2530461f7a8888dbf2b59f2440de228dc2c36d
Akshaj6 <akshaj.samaria@gmail.com>
2025-03-31 13:29:18 +0530

    7

---
+++ Activity for String Operations.py
@@ -0,0 +1,6 @@
+greeting = input("Hello Shyam!!!\n")
+message = "Congratulations for getting a GOLD MEDAL in the State Games!!"
+print(message.upper())
+message_2 = str(input("Did you know what is congratulations spelt backwards?\n"))
+my_string = "congratulations"
+print("Well, it is", my_string[::-1])
